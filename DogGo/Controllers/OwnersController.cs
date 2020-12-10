7574e8d7fed253e56a5f9634ba5b44dba972d3ee
Patchrik <pklly92@gmail.com>
2020-12-10 16:06:37 -0600

    Here's a code push for Adam's bot. Good bot.

---
+++ DogGo/Controllers/OwnersController.cs
@@ -0,0 +1,104 @@
+﻿using DogGo.Models;
+using DogGo.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Controllers
+{
+    public class OwnersController : Controller
+    {
+        private readonly OwnerRepository _ownerRepo;
+
+        public OwnersController(IConfiguration config)
+        {
+            _ownerRepo = new OwnerRepository(config);
+        }
+
+        // GET: OwnerController
+        public ActionResult Index()
+        {
+            List<Owner> owners = _ownerRepo.GetAllOwners();
+            return View(owners);
+        }
+
+        // GET: OwnerController/Details/5
+        public ActionResult Details(int id)
+        {
+            Owner owner = _ownerRepo.GetOwnerById(id);
+
+            if (owner == null)
+            {
+                return NotFound();
+            }
+            return View(owner);
+        }
+
+        // GET: OwnerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OwnerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OwnerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OwnerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OwnerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OwnerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
