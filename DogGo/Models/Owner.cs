51c597443c9bdbc1776f831d2844901d1f8db731
Patchrik <pklly92@gmail.com>
2020-12-17 20:00:54 -0600

    Made a few ui changes

--- DogGo/Models/Owner.cs
+++ DogGo/Models/Owner.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +10,27 @@ namespace DogGo.Models
     public class Owner
     {
         public int Id { get; set; }
+
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
+
+        [Required(ErrorMessage ="You will need a name.")]
+        [MaxLength(80)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(80, MinimumLength = 5)]
         public string Address { get; set; }
+
+        [Required]
+        [DisplayName("Neighborhood")]
         public int NeighborhoodId { get; set; }
+
+        [Required]
+        [DisplayName("Phone Number")]
         public string Phone { get; set; }
+
         public Neighborhood Neighborhood { get; set; }
     }
 }
